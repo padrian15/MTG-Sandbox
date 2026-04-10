833a944a742771d96e7afa056764c26fde2cda08
padrian15 <paadrian@adrianhomes.com>
2026-04-09 21:15:36 -0400

    Zone alignment fix: remove battlefield padding, opponent zones match player zones

--- sandbox/backend/server.js
+++ sandbox/backend/server.js
@@ -11,7 +11,7 @@ const io = new Server(server, {
 });
 
 // Serve the frontend
-app.use(express.static(path.join(__dirname, '..')));
+app.use(express.static(path.join(__dirname, '.')));
 
 const rooms = {};
 
